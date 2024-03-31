6d93e341d938b70bae38edc38e3272cf7651d0b4
shlomtzion mendlovich <shlomtzosh@gmail.com>
2024-03-31 18:03:42 +0300

    31.03

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,4 @@
-
+import Chart from 'chart.js/auto';
 class Coin {
   id: string;
   symbol: string;
@@ -426,25 +426,34 @@ function saveChangesToggleObject() {
 
 saveChanges.addEventListener("click",saveChangesToggleObject)
 
+// let chartElement = document.querySelector("#myChart") as ChartItem
+// (async function() {
+//   const data = [
+//     { year: 2010, count: 10 },
+//     { year: 2011, count: 20 },
+//     { year: 2012, count: 15 },
+//     { year: 2013, count: 25 },
+//     { year: 2014, count: 22 },
+//     { year: 2015, count: 30 },
+//     { year: 2016, count: 28 },
+//   ];
 
+//   new Chart(
+//    chartElement,
+//     {
+//       type: 'bar',
+//       data: {
+//         labels: data.map(row => row.year),
+//         datasets: [
+//           {
+//             label: 'Acquisitions by year',
+//             data: data.map(row => row.count)
+//           }
+//         ]
+//       }
+//     }
+//   );
+// })();
+ 
 
-	var chart = new CanvasJS.Chart("chartContainer", {
-		title:{
-			text: "My First Chart in CanvasJS"              
-		},
-		data: [              
-		{
-			// Change type to "doughnut", "line", "splineArea", etc.
-			type: "column",
-			dataPoints: [
-				{ label: "apple",  y: 30  },
-				{ label: "orange", y: 15  },
-				{ label: "banana", y: 25  },
-				{ label: "mango",  y: 30  },
-				{ label: "grape",  y: 28  }
-			]
-		}
-		]
-	});
-	chart.render();
 
